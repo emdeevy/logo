26c8ff4b5d794025b6337f9cdc5300a75e7f759c
Denis Makula <denis@emdeevy.me>
2023-10-10 05:16:02 +0000

    get the right X and Ys and a bit of work on the throttle

--- js/mouse.js
+++ js/mouse.js
@@ -1,29 +1,33 @@
-const svg = document.getElementById('main').getBoundingClientRect();
+document.addEventListener('DOMContentLoaded', () => {
 
-const masks = document.querySelectorAll('.mover');
-const colors = document.querySelectorAll('#dynamic polygon');
-const polygons = document.querySelectorAll('#polygons polygon');
-const attacker = document.getElementsByClassName('attack-container');
+    const svg = document.getElementById('main').getBoundingClientRect();
 
-let timeout;
+    const masks = document.querySelectorAll('.mover');
+    const colors = document.querySelectorAll('#dynamic polygon');
+    const polygons = document.querySelectorAll('#polygons polygon');
+    const attacker = document.getElementById('attack-container');
 
-attacker.addEventListener('mousemove', (event) => {
-    const windowWidth = svg.width;
-    const windowHeight = svg.height;
+    let timeout;
 
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    const x = (1000 * mouseX) / windowWidth;
-    const y = (1000 * mouseY) / windowHeight;
+    attacker.addEventListener('mousemove', (event) => {
+        const realWidth = svg.width;
+        const realHeight = svg.height;
 
-    masks.forEach(element => {
-        element.setAttribute('cx', x);
-        element.setAttribute('cy', y);
+        const mouseX = event.clientX - svg.left;
+        const mouseY = event.clientY - svg.top;
+        const x = (1000 * mouseX) / realWidth;
+        const y = (1000 * mouseY) / realHeight;
+
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+
+            masks.forEach(element => {
+                element.setAttribute('cx', String(x));
+                element.setAttribute('cy', String(y));
+            });
+
+            console.log(x, y, svg);
+        }, 15);
     });
 
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-        console.log(x, y, svg);
-    }, 200);
 });
-
